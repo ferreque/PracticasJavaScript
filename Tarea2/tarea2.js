321330f0d2d31d13f48c730ddcdd24903ddd4028
Fernando Requena <fernandorequenaserra@gmail.com>
2021-05-02 18:06:30 -0300

    Tarea 2 agregada

---
+++ Tarea2/tarea2.js
@@ -0,0 +1,10 @@
+let nombre= 'josé'
+console.log(`Se puede capitalizar asi: ${nombre.charAt(0).toUpperCase() + nombre.substr(1)}`)
+
+let primerCaracter=nombre.charAt(0)
+console.log(`O asi: ${primerCaracter.toUpperCase() + nombre.substr(1)}`)
+
+console.log(Math.max(1, 156, -48))
+console.log(Math.min(1, 156, -48))
+
+console.log(Math.sqrt(156))
